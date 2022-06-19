e1e4637220939f77be3ecb66a6bbd60f5f64bf4d
tung wei-i <ericliliwayne@gmail.com>
2022-06-19 23:57:37 +0800

    美化星星圖形及乘法表表格及form美化

--- index.php
+++ index.php
@@ -8,16 +8,28 @@
     <style>
         body{
             font-family: 'Courier New', Courier, monospace;
+            background-color: lightgray;
+        }
+        #ninenine{
+            background-color: lightblue;
+        }
+        #print{
+            background-color: lightblue;
         }
         table{
+            font-family: 'Times New Roman', Times, serif;
             margin: 5px auto;
             border: 2px solid black;
             text-align: center;
+            width: 99vw;
             font-size: larger;
+            overflow: scroll;
         }
         tr td{
             border: 1px solid darkslateblue;
             padding: 2px;
+            min-width: 200px;
+            overflow:auto;
         }
         tr:nth-child(odd){
             background-color: lightcyan;
@@ -31,15 +43,33 @@
         .blue{
             color: blue;
         }
+        div{
+            display: block;
+            text-align: left;
+            margin: 10px auto;
+            width: 99vw;
+            height: 50%;
+        }
+        span{
+            font-size: 1.5rem;
+            font-weight: bolder;
+        }
+        .colora{
+            color:cadetblue;
+        }
+        .colorb{
+            color:darkgoldenrod;
+        }
     </style>
 </head>
 <body>
-    <form action="index.php" method="post">
+    <form action="index.php" method="post" id="ninenine">
+        <p>「99」乘法表</p>
         <label for="num1">數字1:
-            <input type="number" name="num1" min="1" max="15" id="num1">
+            <input type="number" name="num1" min="1" max="99" id="num1">
         </label>
         <label for="num2">數字2:
-            <input type="number" name="num2" min="1" max="15" id="num2">
+            <input type="number" name="num2" min="1" max="99" id="num2">
         </label>
         <button type="submit">輸出乘法表</button>
     </form>
@@ -58,112 +88,124 @@
                     $stringpad1 = str_pad($string1,2,"0",STR_PAD_LEFT);
                     $stringpad2 = str_pad($string2,2,"0",STR_PAD_LEFT);
                     $string3 = strval($k);
-                    $stringpad3 = str_pad($string3,3,"0",STR_PAD_LEFT);
+                    $stringpad3 = str_pad($string3,4,"0",STR_PAD_LEFT);
                     $col = (($j%2==1 && $i%2==1) || ($j%2==0 && $i%2==0))?"red":"blue";
-                    echo "<td class=${col}>".$stringpad1." x ".$stringpad2." = ".$stringpad3."</td>";
+                    echo "<td class=${col}>".$stringpad1." x ".$stringpad2." = ".$stringpad3."</td>";   
                 }
                 echo "</tr>";
             }
             echo "</table>";
         }
     ?>
-    <form action="index.php" method="post">
+    <?php
+    $line = isset($_POST['line'])?$_POST['line']:"";
+    $shape = isset($_POST['shape'])?$_POST['shape']:"";
+    ?>
+    <form action="index.php" method="post" id="print">
+    <p>畫星星</p>
         <label for="line">行數:
-            <input type="number" name="line" min="3" max="100" id="line">
+            <input type="number" name="line" min="3" max="101" id="line">
         </label>
         <label for="shape">選擇形狀:
             <select name="shape" id="shape">
-                <option value="直角三角形">直角三角形</option>
-                <option value="倒直角三角形">倒直角三角形</option>
-                <option value="正三角形">正三角形</option>
-                <option value="倒正三角形">倒正三角形</option>
-                <option value="菱形">菱形</option>
-                <option value="矩形">矩形</option>
-                <option value="矩形挖空">矩形挖空</option>
-                <option value="矩形+對角線">矩形+對角線</option>
+                <option value="直角三角形" <?=($shape=='直角三角形')?'selected':'';?>>直角三角形</option>
+                <option value="倒直角三角形" <?=($shape=='倒直角三角形')?'selected':'';?>>倒直角三角形</option>
+                <option value="正三角形" <?=($shape=='正三角形')?'selected':'';?>>正三角形</option>
+                <option value="倒正三角形" <?=($shape=='倒正三角形')?'selected':'';?>>倒正三角形</option>
+                <option value="菱形" <?=($shape=='菱形')?'selected':'';?>>菱形</option>
+                <option value="矩形" <?=($shape=='矩形')?'selected':'';?>>矩形</option>
+                <option value="矩形挖空" <?=($shape=='矩形挖空')?'selected':'';?>>矩形挖空</option>
+                <option value="矩形+對角線" <?=($shape=='矩形+對角線')?'selected':'';?>>矩形+對角線</option>
             </select>
         </label>
         <button type="submit">輸出圖形</button>
     </form>
+    <div>
     <?php
-        $line = isset($_POST['line'])?$_POST['line']:"";
-        $shape = isset($_POST['shape'])?$_POST['shape']:"";
         printstars($line,$shape);
         function printstars($line,$shape){
             switch ($shape) {
                 case '直角三角形':
                     for($i=0;$i<$line;$i++){
+                        $color = ($i%2==1)?'colora':'colorb';
                         for($j=0;$j<=$i;$j++){
-                            echo "*";
+                            echo "<span class=$color>"."*"."</span>";
                         }
                         echo "<br>";
                     }
                     break;
                 case '倒直角三角形':
                     for($i=0;$i<$line;$i++){
+                        $color = ($i%2==1)?'colora':'colorb';
                         for($j=$line;$j>$i;$j--){
-                            echo "*";
+                            echo "<span class=$color>"."*"."</span>";
                         }
                         echo "<br>";
                     }
                     break;
                 case '正三角形':
                     for($i=0;$i<$line;$i++){
+                        $color = ($i%2==1)?'colora':'colorb';
                         for($j=($line-1);$j>$i;$j--){
-                            echo "&nbsp";
+                            echo "<span>&nbsp</span>";
                         }
                         for($k=1;$k<=(2*$i)+1;$k++){
-                            echo "*";
+                            echo "<span class=$color>"."*"."</span>";
                         }
                         echo "<br>";
                     }
                     break;
                 case '倒正三角形':
                     for($i=0;$i<$line;$i++){
+                        $color = ($i%2==1)?'colora':'colorb';
                         for($j=0;$j<=$i;$j++){
-                            echo "&nbsp";
+                            echo "<span>&nbsp</span>";
                         }
                         for($k=($line*2)-1;$k>$i*2;$k--){
-                            echo "*";
+                            echo "<span class=$color>"."*"."</span>";
                         }
                         echo "<br>";
                     }
                     break;
                 case '菱形':
                     for($i=0;$i<$line;$i++){
+                        $color = ($i%2==1)?'colora':'colorb';
                         for($j=($line-1);$j>$i;$j--){
-                            echo "&nbsp";
+                            echo "<span>&nbsp</span>";
                         }
                         for($k=1;$k<=(2*$i)+1;$k++){
-                            echo "*";
+                            echo "<span class=$color>"."*"."</span>";
                         }
                         echo "<br>";
                     }
                     for($i=0;$i<$line;$i++){
+                        $color = ($i%2==0)?'colora':'colorb';
                         for($j=0;$j<=$i;$j++){
-                            echo "&nbsp";
+                            echo "<span>&nbsp</span>";
                         }
                         for($k=(($line-1)*2)-1;$k>$i*2;$k--){
-                            echo "*";
+                            echo "<span class=$color>"."*"."</span>";
                         }
                         echo "<br>";
                     }
                     break;
                 case '矩形':
                     for($i=0;$i<$line;$i++){
+                        $color = ($i%2==1)?'colora':'colorb';
                         for($j=0;$j<$line;$j++){
-                            echo "*"."&nbsp";//多加空格看起來比較方正
+                            echo "<span class=$color>"."*"."</span>"."&nbsp";//多加空格看起來比較方正
                         }
                         echo "<br>";
                     }
                     break;
                 case '矩形挖空':
                     for($i=0;$i<$line;$i++){
+                        $color = ($i%2==1)?'colora':'colorb';
                         for($j=0;$j<$line;$j++){
                             if($i==0 || $i==($line-1) ||$j==0 ||$j==($line-1)){
-                                echo "*"."&nbsp";
+                                echo "<span class=$color>"."*"."</span>"."<span>&nbsp</span>";
                             }else{
-                                echo "&nbsp"."&nbsp";
+                                echo "<span>&nbsp</span>"."<span>&nbsp</span>";
                             }
                         }
                         echo "<br>";
@@ -171,11 +213,12 @@
                     break;
                 case '矩形+對角線':
                     for($i=0;$i<$line;$i++){
+                        $color = ($i%2==1)?'colora':'colorb';
                         for($j=0;$j<$line;$j++){
                             if($i==0 || $i==($line-1) ||$j==0 ||$j==($line-1) ||$i==$j ||$j==(($line-1))-$i){
-                                echo "*"."&nbsp";
+                                echo "<span class=$color>"."*"."</span>"."<span>&nbsp</span>";
                             }else{
-                                echo "&nbsp"."&nbsp";
+                                echo "<span>&nbsp</span>"."<span>&nbsp</span>";
                             }
                         }
                         echo "<br>";
@@ -184,5 +227,6 @@
             }
         }
     ?>
+    </div>
 </body>
 </html>
